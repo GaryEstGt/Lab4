fff35947c6502d5ae28a0cb6d30a1d93231980af
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-04-14 11:41:33 -0600

    m

--- Lab4Proyecto/Lab4Proyecto/Models/Pais.cs
+++ Lab4Proyecto/Lab4Proyecto/Models/Pais.cs
@@ -7,8 +7,8 @@ namespace Lab4Proyecto.Models
 {
     public class Pais
     {
-        public string faltantes { get; set; }
-        public string coleccionadas { get; set; }
-        public string cambios { get; set; }
+        public int[] faltantes { get; set; }
+        public int[] coleccionadas { get; set; }
+        public int[] cambios { get; set; }
     }
 }
